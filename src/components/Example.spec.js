d66f4281126b10c1c8147c0d82b825a08a324daf
Черепахин Василий <vasi.che@gmail.com>
2024-04-12 12:44:01 +0500

    add Example.spec.js

--- src/components/Example.spec.js
+++ src/components/Example.spec.js
@@ -4,6 +4,7 @@ describe("Example test", () => {
   it("test filter by selected props.", () => {
     const selectedProps = ["id", "title", "checked"];
     const exampleTask = {type: "checkbox", id: 100, title: "Task 1", checked: false, label: "100. Task 1"};
+    // label: "100. Task 1" must be removed
     const taskTarget = Object.entries(exampleTask).filter(
         ([k, v]) => selectedProps.includes(k)).reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {});
 
