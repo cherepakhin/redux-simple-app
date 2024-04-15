b8f777eb91f31999490d631ad9f415d78a5d897d
Черепахин Василий <vasi.che@gmail.com>
2024-04-15 19:13:27 +0500

    work DeleteConfirmDlg

--- src/components/Task.js
+++ src/components/Task.js
@@ -11,22 +11,13 @@ const Task = ({ task }) => {
   const dispatch = useDispatch();
 
   const [showDemoModalWarning, setShowDemoModalWarning] = useState(false);
+  const [showDeleteConfirmDlg, setShowDeleteConfirmDlg] = useState(false);
 
 // this business handler
   const handleShowMore= (id) => {
     console.log("show more task id="+id);
 // TODO: show more task
 //    dispatch(createRemoveTaskAction(id));
-  }
-
-// this business handler
-  const handleDeleteTask = (id) => {
-    if(id < 0) {
-      console.log("delete task id="+id);
-      return
-    };
-    console.log("delete task id="+id);
-    dispatch(createRemoveTaskAction(id));
   }
 
 // this demo handler for work SHOW modal dialog
@@ -39,6 +30,58 @@ const Task = ({ task }) => {
   const handleCloseDemoModalWarning = () => {
     console.log("handleCloseModalWarning " + showDemoModalWarning);
     setShowDemoModalWarning(false);
+  }
+
+// this business handler
+//  const handleDeleteTask = (id) => {
+//    if(id < 0) {
+//      console.log("delete task id="+id);
+//      return
+//    };
+//    console.log("handleDeleteTask task id="+id);
+//    dispatch(createRemoveTaskAction(id));
+//  }
+
+//  const handleDeleteConfirmDlg =(id) => {
+//      console.log("function delete task id=" + id);
+//  }
+
+// handler for show DELETE modal dialog
+  const handleDeleteConfirmDlg = (task) => {
+    console.log("before handleShowDeleteConfirmDlg showDeleteConfirmDlg=" + showDeleteConfirmDlg);
+    setShowDeleteConfirmDlg(true);
+    console.log("after handleDeleteConfirmDlg showDeleteConfirmDlg=" + showDeleteConfirmDlg);
+    console.log("delete task id=" + id);
+
+    if(id < 0) {
+      console.log("delete task id=" + id);
+      return
+    };
+// TODO: uncomment for delete task
+//    setShowDeleteConfirmDlg(false);
+//    dispatch(createRemoveTaskAction(id));
+  }
+
+  const handleCloseDeleteConfirmDlg = (task) => {
+    console.log("before handleShowDeleteConfirmDlg showDeleteConfirmDlg=" + showDeleteConfirmDlg);
+    setShowDeleteConfirmDlg(false);
+    console.log("after handleDeleteConfirmDlg showDeleteConfirmDlg=" + showDeleteConfirmDlg);
+    console.log("delete task id=" + id);
+  }
+
+  const handleConfirmDeleteConfirmDlg = (task) => {
+    console.log("before handleConfirmDeleteConfirmDlg showDeleteConfirmDlg=" + showDeleteConfirmDlg);
+    setShowDeleteConfirmDlg(false);
+    console.log("after handleConfirmDeleteConfirmDlg showDeleteConfirmDlg=" + showDeleteConfirmDlg);
+    console.log("delete task id=" + id);
+
+    if(id < 0) {
+      console.log("i<0 delete task id=" + id);
+    } else {
+      console.log("delete task id=" + id);
+      // delete task
+      dispatch(createRemoveTaskAction(id));
+    };
   }
 
   return (
@@ -55,6 +98,21 @@ const Task = ({ task }) => {
         </Modal.Footer>
       </Modal>
 
+      <Modal show={showDeleteConfirmDlg} className="rounded-0">
+        <Modal.Header closeButton>
+          <Modal.Title>Удалить?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{"{id: "+id+", title: '"+title+"'}?"}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleConfirmDeleteConfirmDlg}>
+            Да
+          </Button>
+          <Button variant="primary" onClick={handleCloseDeleteConfirmDlg}>
+            Нет
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       <Form.Check
         id={id}
         type="checkbox"
@@ -66,7 +124,7 @@ const Task = ({ task }) => {
         <span onClick={() => handleShowMore(id)}>Подробнее</span>
       </div>
       <div className="list-group-item-actions" align="center">
-        <span onClick={() => handleDeleteTask(id)}>Удалить</span>
+        <span onClick={() => handleDeleteConfirmDlg(id)}>Удалить</span>
       </div>
       <div className="list-group-item-actions" align="right">
         <span onClick={() => handleShowDemoModalWarning()}>Modal</span>
